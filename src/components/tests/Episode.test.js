085c7e033a9143592db25e7bcf00eb47fc33d70d
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-25 15:26:22 -0400

    new test data, test for image undefined

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,6 +3,16 @@ import { render, fireEvent, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Episode from "./../Episode";
 
+const imageMissing = {
+  id: 1,
+  image: null,
+  name: "",
+  season: 1,
+  number: 1,
+  summary: "test episode",
+  runtime: 1,
+};
+
 const testEpisode = {
   id: 1,
   image: "https://i.ibb.co/2FsfXqM/stranger-things.png",
@@ -27,4 +37,8 @@ test("renders the summary test passed as prop", () => {
   expect(summaryStatement).toBeTruthy();
 });
 
-test("renders default image when image is not defined", () => {});
+test("renders default image when image is not defined", () => {
+  render(<Episode episode={imageMissing} />);
+
+  screen.queryAllByAltText("./stranger_things.png");
+});
