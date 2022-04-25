94eb12a9929bd6ec62f1dc83cba51d634ef5ed12
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-25 16:53:08 -0400

    displaytest display renders no err

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Display from './../Display';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Display from "./../Display";
 
-test('renders without errors with no props', async () => { });
+import mockFetchShow from "./../../api/fetchShow";
+jest.mock("./../../api/fetchShow");
 
-test('renders Show component when the button is clicked ', () => { });
+const exampleData = {
+  name: "test name",
+  summary: "test summary",
+  seasons: [
+    {
+      id: 0,
+      name: "season 1",
+      episodes: [],
+    },
+    {
+      id: 1,
+      name: "season 2",
+      episodes: [],
+    },
+  ],
+};
 
-test('renders show season options matching your data when the button is clicked', () => { });
+test("renders without errors with no props", async () => {
+  render(<Display />);
+});
+
+test("renders Show component when the button is clicked ", () => {});
+
+test("renders show season options matching your data when the button is clicked", () => {});
