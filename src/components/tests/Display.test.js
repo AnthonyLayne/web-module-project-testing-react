3ac3c261a94399dc6b9990a19efc871a70950211
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-25 16:58:48 -0400

    displaytest render show comp when bttn click

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, waitFor, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Display from "./../Display";
 
 import mockFetchShow from "./../../api/fetchShow";
+import userEvent from "@testing-library/user-event";
 jest.mock("./../../api/fetchShow");
 
 const exampleData = {
@@ -27,6 +28,15 @@ test("renders without errors with no props", async () => {
   render(<Display />);
 });
 
-test("renders Show component when the button is clicked ", () => {});
+test("renders Show component when the button is clicked ", async () => {
+  mockFetchShow.mockResolvedValueOnce(exampleData);
+  render(<Display />);
+
+  const bttn = screen.getByRole("button");
+  userEvent.click(bttn);
+
+  const showContain = await screen.findByTestId("show-container");
+  expect(showContain).toBeInTheDocument();
+});
 
 test("renders show season options matching your data when the button is clicked", () => {});
